50a496601e8d796291f4c7f57c171daf5e462e70
Carlos Hernandez <honchkrow1995@gmail.com>
2016-11-10 17:34:17 -0600

    Clean assets
    - compact files and refactor my own templates
    - set on rest of admin pages

--- application/views/backoffice_admin/templates/custom_header.php
+++ application/views/backoffice_admin/templates/custom_header.php
@@ -1,17 +1,28 @@
 <!DOCTYPE html>
 <html ng-app="akamaiposApp" lang="en">
 <head>
+    <title>AK <?php echo (isset($page_title)) ? $page_title: ''?></title>
     <meta charset="utf-8" />
 </head>
-<title>AK <?php echo (isset($page_title)) ? $page_title: ''?></title>
-<script type="text/javascript" src="<?php echo base_url("assets/js/jquery-1.10.2.min.js")?>"></script>
-<script src="<?php echo base_url('assets/js/bootstrap.min.js') ?>"></script>
-<script src="<?php echo base_url('assets/js/custom.js') ?>"></script>
-<script src="<?php echo base_url('assets/js/bootstrapValidator.js')?>"></script>
-<?php hdplugins()?>
-<link href="<?php echo base_url('assets/css/global.css')?>" rel="stylesheet" type="text/css">
-<link href="<?php echo base_url('assets/css/font-awesome.css')?>" rel="stylesheet">
-<link href="<?php echo base_url('assets/css/custom.css')?>" rel="stylesheet">
+<!--<script type="text/javascript" src="--><?php //echo base_url("assets/js/jquery-1.10.2.min.js")?><!--"></script>-->
+<!--<script src="--><?php //echo base_url('assets/js/bootstrap.min.js') ?><!--"></script>-->
+<!--<script src="--><?php //echo base_url('assets/js/custom.js') ?><!--"></script>-->
+<!--<script src="--><?php //echo base_url('assets/js/bootstrapValidator.js')?><!--"></script>-->
+<?php //hdplugins()?>
+<!--<link href="--><?php //echo base_url('assets/css/global.css')?><!--" rel="stylesheet" type="text/css">-->
+<!--<link href="--><?php //echo base_url('assets/css/font-awesome.css')?><!--" rel="stylesheet">-->
+<!--<link href="--><?php //echo base_url('assets/css/custom.css')?><!--" rel="stylesheet">-->
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/jquery-1.12.0.min.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/admin/dist/jqxangular.min.js"></script>
+<!--<link rel="stylesheet" type="text/css" href="--><?php //echo base_url() ?><!--assets/css/global.css" />-->
+<!--<link rel="stylesheet" type="text/css" href="--><?php //echo base_url() ?><!--assets/css/custom.css" />-->
+<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/admin/dist/jqxstyles.min.css" />
+<!--<link rel="stylesheet" type="text/css" href="--><?php //echo base_url() ?><!--assets/js/angular/jqwidgets/styles/jqx.base.css" />-->
+<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/js/angular/jqwidgets/styles/jqxthemes.min.css" />
+<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/admin/styles.css">
+<script type="text/javascript">
+    var SiteRoot ="<?php echo base_url() ?>";
+</script>
 <style type="text/css">
     body{
         color: #000;
