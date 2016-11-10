4c58b3c3a040e4f489f857805354faf13eef2153
Carlos Hernandez <honchkrow1995@gmail.com>
2016-11-09 19:52:53 -0600

    Brands #202
    - clean

--- application/views/backoffice_admin/menu_categories/inventory/brands/info_subtab.php
+++ application/views/backoffice_admin/menu_categories/inventory/brands/info_subtab.php
@@ -3,8 +3,6 @@
         <div style="float:left; padding:2px;">
             <div style="float:left; padding:8px; text-align:right; width:125px; font-weight:bold;">Created:</div>
             <div style="float:left; width:320px;">
-<!--                <input type="text" id="ibrands_created" class="form-control"-->
-<!--                       ng-disabled="disabled"/>-->
                 <jqx-date-time-input id="ibrands_created" class=""
                                      ng-disabled="disabled" jqx-width="320"
                 ></jqx-date-time-input>
@@ -13,8 +11,6 @@
         <div style="float:left; padding:2px; ">
             <div style="float:left; padding:8px; text-align:right; width:125px; font-weight:bold;">Created By:</div>
             <div style="float:left; width:320px;">
-<!--                <input type="text" id="ibrands_createdbyname" class="form-control"-->
-<!--                       ng-disabled="disabled">-->
                 <jqx-combo-box id="ibrands_createdbyname" class=""
                                jqx-disabled="disabled" jqx-width="320">
                 </jqx-combo-box>
@@ -23,8 +19,6 @@
         <div style="float:left; padding:2px;">
             <div style="float:left; padding:8px; text-align:right; width:125px; font-weight:bold;">Updated:</div>
             <div style="float:left; width:320px;">
-<!--                <input type="text" id="ibrands_updated" class="form-control"-->
-<!--                       ng-disabled="disabled"/>-->
                 <jqx-date-time-input id="ibrands_updated" class=""
                                      ng-disabled="disabled" jqx-width="320"
                 ></jqx-date-time-input>
@@ -33,8 +27,6 @@
         <div style="float:left; padding:2px;">
             <div style="float:left; padding:8px; text-align:right; width:125px; font-weight:bold;">Updated By:</div>
             <div style="float:left; width:320px;">
-<!--                <input type="text" class="form-control"-->
-<!--                       id="ibrands_updatedbyname" ng-disabled="disabled">-->
                 <jqx-combo-box id="ibrands_updatedbyname" class=""
                                jqx-disabled="disabled" jqx-width="320">
                 </jqx-combo-box>
