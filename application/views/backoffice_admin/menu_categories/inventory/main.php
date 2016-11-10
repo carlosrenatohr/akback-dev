1a201ee321c5050973f66dfb7e354491b770f863
Carlos Hernandez <honchkrow1995@gmail.com>
2016-11-09 20:28:02 -0600

    Item Grid --> Put Pager on Top #212
    - design changes

--- application/views/backoffice_admin/menu_categories/inventory/main.php
+++ application/views/backoffice_admin/menu_categories/inventory/main.php
@@ -27,9 +27,10 @@ jqxthemes();
 <link rel="stylesheet" href="<?php echo base_url() ?>assets/admin/styles.css">
 <link rel="stylesheet" href="<?php echo base_url() ?>assets/js/jqwidgets/styles/jqx.summer.css" type="text/css" />
 
-<div class="container-fluid" ng-controller="menuItemsInventoryController">
+<div class="container-fluid" id="inventorySection"
+     ng-controller="menuItemsInventoryController" style="padding: 0;">
 
-    <div class="row">
+    <div class="">
         <nav class="navbar navbar-default" role="navigation" style="background:#CCC;">
             <div class="col-md-12">
                 <div id="toolbar" class="toolbar-list">
@@ -58,14 +59,13 @@ jqxthemes();
         </nav>
     </div>
 
-    <div class="row">
-        <div class="col-md-12">
-            <jqx-grid id="inventoryItemsGrid"
-                      jqx-settings="inventoryItemsGrid"
-                      jqx-on-rowdoubleclick="editInventoryWind(e)"
-                      jqx-create="inventoryItemsGrid"
-            ></jqx-grid>
-        </div>
+    <div class="">
+        <jqx-grid id="inventoryItemsGrid"
+                  jqx-settings="inventoryItemsGrid"
+                  jqx-on-rowdoubleclick="editInventoryWind(e)"
+                  jqx-create="inventoryItemsGrid"
+        ></jqx-grid>
+        <!-- - -->
         <jqx-window jqx-on-close="closeInventoryWind($event)" jqx-settings="itemsInventoryWindowSettings"
                     jqx-create="itemsInventoryWindowSettings" id="invMainWindow">
             <div>
@@ -135,22 +135,20 @@ jqxthemes();
 
                 <!-- Main buttons before saving item on grid -->
                 <div class="col-md-12 col-md-offset-0">
-                    <div class="row">
-                        <div id="mainButtonsOnItemInv" class="rowMsgInv">
-                            <div class="form-group">
-                                <div class="col-sm-12">
-                                    <span class="btn btn-success" id="uploadItemPictureBtn" flow-btn style="display: none;">
-                                        Upload Picture
-                                    </span>
-                                    <button type="button" id="saveInventoryBtn" ng-click="saveInventoryAction()"
-                                            class="btn btn-primary" disabled>
-                                        Save
-                                    </button>
-                                    <button type="button" ng-click="closeInventoryAction()" class="btn btn-warning">Close</button>
-                                    <button type="button" id="deleteInventoryBtn" ng-click="deleteInventoryAction()" class="btn btn-danger"
-                                            style="overflow:auto;display: none;">Delete
-                                    </button>
-                                </div>
+                    <div id="mainButtonsOnItemInv" class="rowMsgInv">
+                        <div class="form-group">
+                            <div class="col-sm-12">
+                                <span class="btn btn-success" id="uploadItemPictureBtn" flow-btn style="display: none;">
+                                    Upload Picture
+                                </span>
+                                <button type="button" id="saveInventoryBtn" ng-click="saveInventoryAction()"
+                                        class="btn btn-primary" disabled>
+                                    Save
+                                </button>
+                                <button type="button" ng-click="closeInventoryAction()" class="btn btn-warning">Close</button>
+                                <button type="button" id="deleteInventoryBtn" ng-click="deleteInventoryAction()" class="btn btn-danger"
+                                        style="overflow:auto;display: none;">Delete
+                                </button>
                             </div>
                         </div>
                     </div>
@@ -158,29 +156,25 @@ jqxthemes();
 
                 <!-- Prompt before saving item on grid -->
                 <div class="col-md-12 col-md-offset-0">
-                    <div class="row">
-                        <div id="promptCloseItemInv" class="rowMsgInv" style="display: none">
-                            <div class="form-group">
-                                <div class="col-sm-12">
-                                    <div class="message">Do you want to save your changes?</div>
-                                    <button type="button" ng-click="closeInventoryAction(0)" class="btn btn-primary">Yes</button>
-                                    <button type="button" ng-click="closeInventoryAction(1)" class="btn btn-warning">No</button>
-                                    <button type="button" ng-click="closeInventoryAction(2)" class="btn btn-info">Cancel</button>
-                                </div>
+                    <div id="promptCloseItemInv" class="rowMsgInv" style="display: none">
+                        <div class="form-group">
+                            <div class="col-sm-12">
+                                <div class="message">Do you want to save your changes?</div>
+                                <button type="button" ng-click="closeInventoryAction(0)" class="btn btn-primary">Yes</button>
+                                <button type="button" ng-click="closeInventoryAction(1)" class="btn btn-warning">No</button>
+                                <button type="button" ng-click="closeInventoryAction(2)" class="btn btn-info">Cancel</button>
                             </div>
                         </div>
                     </div>
                 </div>
                 <!-- Prompt before saving item to add features on grid -->
                 <div class="col-md-12 col-md-offset-0">
-                    <div class="row">
-                        <div id="promptMoveItemInv" class="rowMsgInv" style="display: none">
-                            <div class="form-group">
-                                <div class="col-sm-12">
-                                    <div class="message">To add a <label id="tabTitleOnMsg"></label> new item must first be saved. Do you want to save now ?</div>
-                                    <button type="button" ng-click="moveTabInventoryAction(0)" class="btn btn-primary">Yes</button>
-                                    <button type="button" ng-click="moveTabInventoryAction(2)" class="btn btn-info">Cancel</button>
-                                </div>
+                    <div id="promptMoveItemInv" class="rowMsgInv" style="display: none">
+                        <div class="form-group">
+                            <div class="col-sm-12">
+                                <div class="message">To add a <label id="tabTitleOnMsg"></label> new item must first be saved. Do you want to save now ?</div>
+                                <button type="button" ng-click="moveTabInventoryAction(0)" class="btn btn-primary">Yes</button>
+                                <button type="button" ng-click="moveTabInventoryAction(2)" class="btn btn-info">Cancel</button>
                             </div>
                         </div>
                     </div>
@@ -188,15 +182,13 @@ jqxthemes();
 
                 <!-- Prompt before delete an item on grid -->
                 <div class="col-md-12 col-md-offset-0">
-                    <div class="row">
-                        <div id="promptToDeleteItemInv" class="rowMsgInv" style="display: none">
-                            <div class="form-group">
-                                <div class="col-sm-12">
-                                    Do you really want to delete it?
-                                    <button type="button" ng-click="deleteInventoryAction(0)" class="btn btn-primary">Yes</button>
-                                    <button type="button" ng-click="deleteInventoryAction(1)" class="btn btn-warning">No</button>
-                                    <button type="button" ng-click="deleteInventoryAction(2)" class="btn btn-info">Cancel</button>
-                                </div>
+                    <div id="promptToDeleteItemInv" class="rowMsgInv" style="display: none">
+                        <div class="form-group">
+                            <div class="col-sm-12">
+                                Do you really want to delete it?
+                                <button type="button" ng-click="deleteInventoryAction(0)" class="btn btn-primary">Yes</button>
+                                <button type="button" ng-click="deleteInventoryAction(1)" class="btn btn-warning">No</button>
+                                <button type="button" ng-click="deleteInventoryAction(2)" class="btn btn-info">Cancel</button>
                             </div>
                         </div>
                     </div>
@@ -241,6 +233,9 @@ jqxthemes();
     }
     .row.inventory_tab {
         margin: 0!important;
+    }
+    #inventorySection .row {
+        /*margin: 0!important;*/
     }
 </style>
 <?php $this->load->view('backoffice_includes/backoffice_mainmenu_footer'); ?>
