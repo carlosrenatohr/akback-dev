795ebb25b660acab7ee4705c20ffb374b9e4063c
Carlos Hernandez <honchkrow1995@gmail.com>
2016-12-24 17:02:01 -0600

    User --> Email Changes #250

--- application/views/backoffice_admin/users/contact_tab.php
+++ application/views/backoffice_admin/users/contact_tab.php
@@ -1,49 +1,49 @@
 <div class="row">
     <div style="width:420px;float:left;">
         <div style="float:left; padding:2px; width:420px;">
-            <div style="float:left; padding:8px; text-align:right; width:150px; font-weight:bold;">Personal Email</div>
+            <div style="float:left; padding:8px; text-align:right; width:160px; font-weight:bold;">Personal Email</div>
             <div style="float:left; width:220px;">
                 <input type="email" class="form-control addUserField" id="add_emailpersonal" name="add_emailPersonal" placeholder="Personal Email">
             </div>
         </div>
 
         <div style="float:left; padding:2px; width:420px;">
-            <div style="float:left; padding:8px; text-align:right; width:150px; font-weight:bold;">Address 1:</div>
+            <div style="float:left; padding:8px; text-align:right; width:160px; font-weight:bold;">Address 1:</div>
             <div style="float:left; width:220px;">
                 <input type="text" class="form-control addUserField" id="add_address1" name="add_address1" placeholder="Adress1">
             </div>
         </div>
 
         <div style="float:left; padding:2px; width:420px;">
-            <div style="float:left; padding:8px; text-align:right; width:150px; font-weight:bold;">Address 2:</div>
+            <div style="float:left; padding:8px; text-align:right; width:160px; font-weight:bold;">Address 2:</div>
             <div style="float:left; width:220px;">
                 <input type="text" class="form-control addUserField" id="add_address2" name="add_address2" placeholder="Adress2">
             </div>
         </div>
 
         <div style="float:left; padding:2px; width:420px;">
-            <div style="float:left; padding:8px; text-align:right; width:150px; font-weight:bold;">City:</div>
+            <div style="float:left; padding:8px; text-align:right; width:160px; font-weight:bold;">City:</div>
             <div style="float:left; width:220px;">
                 <input type="text" class="form-control addUserField" id="add_city" name="add_city" placeholder="City">
             </div>
         </div>
 
         <div style="float:left; padding:2px; width:420px;">
-            <div style="float:left; padding:8px; text-align:right; width:150px; font-weight:bold;">State:</div>
+            <div style="float:left; padding:8px; text-align:right; width:160px; font-weight:bold;">State:</div>
             <div style="float:left; width:220px;">
                 <input type="text" class="form-control addUserField" id="add_state" name="add_state" placeholder="State">
             </div>
         </div>
 
         <div style="float:left; padding:2px; width:420px;">
-            <div style="float:left; padding:8px; text-align:right; width:150px; font-weight:bold;">Zip:</div>
+            <div style="float:left; padding:8px; text-align:right; width:160px; font-weight:bold;">Zip:</div>
             <div style="float:left; width:220px;">
                 <input type="text" class="form-control addUserField" id="add_zip" name="add_zip" placeholder="Zip">
             </div>
         </div>
 
         <div style="float:left; padding:2px; width:420px;">
-            <div style="float:left; padding:8px; text-align:right; width:150px; font-weight:bold;">Country:</div>
+            <div style="float:left; padding:8px; text-align:right; width:160px; font-weight:bold;">Country:</div>
             <div style="float:left; width:220px;">
                 <input type="text" class="form-control addUserField" id="add_country" name="add_zip" placeholder="Country">
             </div>
