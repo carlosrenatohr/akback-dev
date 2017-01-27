bca429f95b4552393c9fa033dbc3ae99aa93032d
Carlos Hernandez <honchkrow1995@gmail.com>
2017-01-26 23:44:34 -0600

    Menu Items --> Add Styling Options #272
    - style changes

--- application/views/backoffice_admin/menu_categories/items_style_subtab.php
+++ application/views/backoffice_admin/menu_categories/items_style_subtab.php
@@ -1,8 +1,8 @@
 <div id="styling_menu_items_section">
 
     <div style="float:left; padding:2px; width:450px;">
-        <div style="float:left; padding:8px; text-align:right; width:180px; font-weight:bold;">Button Primary Color:</div>
-        <div style="float:left; width:180px;">
+        <div style="float:left; padding:8px; text-align:right; width:200px; font-weight:bold;">Button Primary Color:</div>
+        <div style="float:left; width:200px;">
             <div style="margin: 3px; float: left;" class="dropDownParent">
                 <jqx-drop-down-button jqx-on-opening="qOpening(event)" jqx-width="150" jqx-instance="ddb_mitbPrimaryColor"
                                       id="ddb_mitbPrimaryColor" class="styles-control" jqx-height="22">
@@ -15,8 +15,8 @@
     </div>
 
     <div style="float:left; padding:2px; width:450px;">
-        <div style="float:left; padding:8px; text-align:right; width:180px; font-weight:bold;">Button Secondary Color:</div>
-        <div style="float:left; width:180px;">
+        <div style="float:left; padding:8px; text-align:right; width:200px; font-weight:bold;">Button Secondary Color:</div>
+        <div style="float:left; width:200px;">
             <div style="margin: 3px; float: left;">
                 <jqx-drop-down-button jqx-on-opening="qOpening(event)" jqx-width="150" jqx-instance="ddb_mitbSecondaryColor"
                                       id="ddb_mitbSecondaryColor" class="styles-control" jqx-height="22">
@@ -29,8 +29,8 @@
     </div>
 
     <div style="float:left; padding:2px; width:450px;">
-        <div style="float:left; padding:8px; text-align:right; width:180px; font-weight:bold;">Label Font Color</div>
-        <div style="float:left; width:180px;">
+        <div style="float:left; padding:8px; text-align:right; width:200px; font-weight:bold;">Label Font Color</div>
+        <div style="float:left; width:200px;">
             <div style="margin: 3px; float: left;">
                 <jqx-drop-down-button jqx-on-opening="qOpening(event)" jqx-width="150" jqx-instance="ddb_mitlfontColor"
                                       id="ddb_mitlfontColor" jqx-height="22" class="styles-control">
@@ -43,9 +43,9 @@
     </div>
 
     <div style="float:left; padding:2px; width:450px;">
-        <div style="float:left; padding:8px; text-align:right; width:180px; font-weight:bold;">Label Font Size</div>
+        <div style="float:left; padding:8px; text-align:right; width:200px; font-weight:bold;">Label Font Size</div>
         <div style="float:left; width:180px;">
-            <jqx-drop-down-list id="mitlfontSize" class="styles-control">
+            <jqx-drop-down-list id="mitlfontSize" class="styles-control" jqx-width="150">
                 <option value="4px">4px</option>
                 <option value="6px">6px</option>
                 <option value="8px">8px</option>
